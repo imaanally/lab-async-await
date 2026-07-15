e4cf611e3b73c36a8a391bb98f32941620a55326
imaan <imaanally123@gmail.com>
2026-07-15 17:55:46 +0300

    Fix getPosts function scope

--- index.js
+++ index.js
@@ -3,27 +3,26 @@
 const postList = document.getElementById("post-list");
 
 function displayPosts(posts) {
-    posts.forEach(function (post) {
-        const postItem = document.createElement("li");
-        const postTitle = document.createElement("h1");
-        const postBody = document.createElement("p");
+  posts.forEach(function (post) {
+    const postItem = document.createElement("li");
+    const postTitle = document.createElement("h1");
+    const postBody = document.createElement("p");
 
-        postTitle.textContent = post.title;
-        postBody.textContent = post.body;
+    postTitle.textContent = post.title;
+    postBody.textContent = post.body;
 
-        postItem.appendChild(postTitle);
-        postItem.appendChild(postBody);
+    postItem.appendChild(postTitle);
+    postItem.appendChild(postBody);
 
-        postList.appendChild(postItem);
-    });
+    postList.appendChild(postItem);
+  });
+}
 
-    async function getPosts() {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts",);
-        const posts = await response.json();
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
 
-        displayPosts(posts);
-    }
+  displayPosts(posts);
 }
 
 getPosts();
