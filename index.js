9e64ad45930b7edb2453b197399406bd0384a1b8
imaan <imaanally123@gmail.com>
2026-07-15 16:10:31 +0300

    Implement API fetch and display social media posts

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+
+const postList = document.getElementById("post-list");
+
+function displayPosts(posts) {
+    posts.forEach(function (post) {
+        const postItem = document.createElement("li");
+        const postTitle = document.createElement("h1");
+        const postBody = document.createElement("p");
+
+        postTitle.textContent = post.title;
+        postBody.textContent = post.body;
+
+        postItem.appendChild(postTitle);
+        postItem.appendChild(postBody);
+
+        postList.appendChild(postItem);
+    });
+
+    async function getPosts() {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts",);
+        const posts = await response.json();
+
+        displayPosts(posts);
+    }
+}
+
+getPosts();
